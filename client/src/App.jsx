8c1fc1054ccd4b480e699945a2287cc3eff08bf8
himanshuchaudhari969 <brahmajatva@gmail.com>
2026-06-07 09:41:43 +0530

    update server url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://p2p-fileshare-si8x.onrender.com");
 
 function App() {
   const [roomId, setRoomId] = useState("");
