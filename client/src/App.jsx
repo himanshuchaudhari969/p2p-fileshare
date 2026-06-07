9ac918780012c673da07e3ac4a1674f885d55fce
himanshuchaudhari969 <brahmajatva@gmail.com>
2026-06-07 21:46:55 +0530

    better TURN servers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,12 +60,15 @@ function App() {
 
   const startWebRTC = async (initiator) => {
     const peer = new RTCPeerConnection({
-      iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-        { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" },
-        { urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject" },
-      ],
+    iceServers: [
+  { urls: "stun:stun.l.google.com:19302" },
+  { urls: "stun:stun1.l.google.com:19302" },
+  { urls: "stun:stun2.l.google.com:19302" },
+  { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" },
+  { urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject" },
+  { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" },
+  { urls: "turn:openrelay.metered.ca:5349", username: "openrelayproject", credential: "openrelayproject" },
+],
     });
     peerRef.current = peer;
     peer.onicecandidate = (e) => {
